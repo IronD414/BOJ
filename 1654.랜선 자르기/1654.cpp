6f2a28f65166a2e15d467896ef06d2d1f8a30ec2
2020073190 <chdragon01@hanyang.ac.kr>
2023-09-04 18:55:30 +0900

    1654 success

--- 1654.랜선 자르기/1654.cpp
+++ 1654.랜선 자르기/1654.cpp
@@ -1,37 +1,59 @@
 #include <iostream>
+#include <random>
 using namespace std;
 
-int cut_made(int *l, int n, int c)
+bool is_abletocut(int size, int *a, long long m, int n)
 {
-	int amount = 0;
-	for (int i = 0; i < n; ++i)
+	long long sum = 0;
+	for (int i = 0; i < size; ++i)
 	{
-		amount += l[i] / c;
+		sum += a[i] / m;
 	}
-	return amount;
+	return sum >= n;
 }
 
 int main(int argc, char const *argv[])
 {
-	int k, n, key_input;
+	int k, n;
 	int *lan_cables;
-	long sum = 0; int cut;
+	long long sum = 0, key_input, mid, start = 0, end, result;
 
-	cin >> k >> n;
-	lan_cables = new int[k];
-	for (int i = 0; i < k; ++i)
-	{
-		cin >> key_input;
-		sum += key_input;
-		lan_cables[i] = key_input;
-	}
-	cut = sum / n;
-	while (cut_made(lan_cables, k, cut) < n)
-	{
-		cut--;
-	}
-	cout << cut;
+	// for (int testcase = 0; testcase < 100; testcase++)
+	// {
+		k = rand() % 10000 + 1;
+		n = rand() % (30000 - k - 1) + k;
+		cin >> k >> n;
+		lan_cables = new int[k];
+		for (int i = 0; i < k; ++i)
+		{
+			// key_input = rand() % 30000 + 1;
+			cin >> key_input;
+			sum += key_input;
+			lan_cables[i] = key_input;
+		}
+		end = sum / n;
+		mid = (start + end) / 2;
 
+		if (is_abletocut(k, lan_cables, end, n)) result = end;
+		else 
+		{
+			while (start < end)
+			{
+				if (is_abletocut(k, lan_cables, mid, n))
+				{
+					start = mid + 1;
+				}else
+				{
+					end = mid - 1;
+				}
+				mid = (start + end) / 2;
+			}
+			result = mid;
+		}
+		if (!is_abletocut(k, lan_cables, result, n)) result--;
+		cout << result;
+		// cout << "k: " << k << ", n: " << n << ", result: " << result << "\n======" << endl;
+	// }
 	delete[] lan_cables;
 	return 0;
 }
